edbbaeed83fdd0315931b946cdf88977cb4046b4
Stefan Tschurtschenthaler <stefan.tschurtsch@gmail.com>
2022-07-09 13:02:44 +0200

    Commit of empty project

---
+++ src/extension.ts
@@ -0,0 +1,25 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "navigation-module-search" is now active!');
+
+	let disposable = vscode.window.setStatusBarMessage('Never saved anything');
+	context.subscriptions.push(disposable);
+
+	let disposable2 = vscode.workspace.onDidSaveTextDocument((evt) => {
+  		const disposable3 = vscode.window.setStatusBarMessage(`Saved ${evt.fileName} at ${Date.now()}`, 1000);
+  		context.subscriptions.push(disposable3);
+	});
+
+	context.subscriptions.push(disposable2);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
